613deaefb7e139ac0a5176b18fcd0821be3e5c05
Dan <D.Ward2@exeter.ac.uk>
2022-08-08 22:24:15 +0100

    Added divs for nav bar

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import dash
+from dash import html, dcc, Input, Output, State
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = dash.Dash(__name__)
+server = app.server
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                    html.Div(children=[html.Div(style={'width': '20%', 'height': '20%', 'margin': 'auto', 'background-color': 'red'}),
+                    html.Div(style={'width': '20%', 'height': '20%', 'margin': 'auto', 'background-color': 'red'}),
+                    html.Div(style={'width': '20%', 'height': '20%', 'margin': 'auto', 'background-color': 'red'}),
+                    html.Div(style={'width': '20%', 'height': '20%', 'margin': 'auto', 'background-color': 'red'}),], style={'height': '30%', 'width': '80%', 'background-color': 'blue', 'margin': 'auto'}),
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+                    #html.Div(style={'width': '20%', 'height': '20%', 'margin': 'auto', 'background-color': 'red'}),
+                    #html.Div(style={'width': '20%', 'height': '20%', 'margin': 'auto', 'background-color': 'red'}),
+                    #html.Div(style={'width': '20%', 'height': '20%', 'margin': 'auto', 'background-color': 'red'}),
+                    #html.Div(style={'width': '20%', 'height': '20%', 'margin': 'auto', 'background-color': 'red'}),
+
+
+                    #html.H3("Home", style={'color': 'white', 'position': 'absolute', 'left': '25vw', 'top': '25%'}),
+                    #html.H3("Upload", style={'color': 'white', 'position': 'absolute', 'left': '40vw'}),
+                    #html.H3("Cleaning and Data Summary", style={'color': 'white', 'position': 'absolute', 'left': '60vw'}),
+                    #html.H3("Network Visualisation", style={'color': 'white', 'position': 'absolute', 'left': '75vw'})
+                      ],
+            style={'width': '100vw', 'height': '20vh', 'background-color': '#000000', 'position': 'fixed'}
+        ),
+
+
+
+        html.H1("Hello World", style={'height': '110vh', 'position': 'relative', 'top': '25vh'})
+
+    ]
+
+
+)
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run_server(debug=True)
